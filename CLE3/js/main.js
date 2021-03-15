da1e78a83e2d5b5e7cffad678b3eac3417883c48
Gerri-Eickhof <knakkerf@users.noreply.github.com>
2021-03-15 10:06:47 +0100

    Structuur

--- CLE3/js/main.js
+++
@@ -1,8 +0,0 @@
-//Variabelen en datatypes
-const artiest = "Megadeth"; //string
-const album = "Rust In Peace";
-let grammy = true;          //boolean
-let duration = 60;          //num
-let tracks = ["Tornado of Souls", "Holy Wars", "Peace Sells"]; //array
-
-
