d0b6f3773569a19caf3a2c712b842dff54abee34
TimoWieme <1004149@hr.nl>
2021-03-15 13:09:48 +0100

    Db connection added.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-document.getElementById('pindakaas')
+window.addEventListener('load', init);
+
+function init()
+{
+
+}
+
+
+function createPlayField()
+{
+    for (let i = 0; i < 4; i++) {
+        let playfield = document.getElementById('playing-field');
+
+        let card = document.createElement('div');
+        card.classList.add('playing-card');
+        playfield.appendChild(card);
+
+        let img = document.createElement('img');
+        img.setAttribute('src', './img/vraagteken-plaatjes.png')
+        // img.src = './img/vraagteken-plaatjes.png'
+        card.appendChild(img)
+
+        let title = document.createElement('h2');
+        title.innerText = i
+        card.appendChild(title)
+    }
+}
