6a6bc62214445553fca8479d8ff5ad86d6630ae0
Gerri-Eickhof <74653523+Gerri-Eickhof@users.noreply.github.com>
2021-03-15 09:57:41 +0100

    Add files via upload

---
+++ CLE3/js/main.js
@@ -0,0 +1,8 @@
+//Variabelen en datatypes
+const artiest = "Megadeth"; //string
+const album = "Rust In Peace";
+let grammy = true;          //boolean
+let duration = 60;          //num
+let tracks = ["Tornado of Souls", "Holy Wars", "Peace Sells"]; //array
+
+
