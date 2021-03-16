aaa88d0a9b4f879951d6d054890849590e2fd263
TimoWieme <1004149@hr.nl>
2021-03-16 16:11:49 +0100

    Login page added

--- register.php
+++ register.php
@@ -3,15 +3,15 @@
 require_once "included/db-connection.php";
 
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
+$username = $password = $confirm_password = $avatar = "";
+$username_err = $password_err = $confirm_password_err = $avatar_err = "";
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     // Validate username
     if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
+        $username_err = "Vul een gebruikersnaam in.";
     } else{
         // Prepare a select statement
         $sql = "SELECT id FROM users WHERE username = ?";
@@ -29,12 +29,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 mysqli_stmt_store_result($stmt);
 
                 if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
+                    $username_err = "Deze gebruikersnaam is al in gebruik.";
                 } else{
                     $username = trim($_POST["username"]);
                 }
             } else{
-                echo "Oops! Something went wrong. Please try again later.";
+                echo "Oops! Er ging iets mis, probeer het opnieuw.";
             }
 
             // Close statement
@@ -44,36 +44,45 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     // Validate password
     if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";
+        $password_err = "Voer een wachtwoord in.";
     } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have atleast 6 characters.";
+        $password_err = "Het wachtwoord moet minstens 6 tekens bevatten.";
     } else{
         $password = trim($_POST["password"]);
     }
 
     // Validate confirm password
     if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";
+        $confirm_password_err = "Bevestig uw wachtwoord.";
     } else{
         $confirm_password = trim($_POST["confirm_password"]);
         if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
+            $confirm_password_err = "Het wachtwoord komt niet overeen.";
         }
+
+        // Validate profile picture
+        if(empty(trim($_POST["avatar"]))){
+            $avatar_err = "Voeg een profiel foto toe.";
+        } else{
+            $avatar = trim($_POST["avatar"]);
+        }
+
     }
 
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)&& empty($avatar_err)){
 
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username, password, profile_picture) VALUES (?, ?, ?)";
 
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password, $param_avatar);
 
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_avatar = $avatar;
 
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
@@ -120,6 +129,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <label>Confirm Password</label>
             <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
             <span class="help-block"><?php echo $confirm_password_err; ?></span>
+        </div>
+        <div class="avatar <?php echo (!empty($avatar_err)) ? 'has-error' : ''; ?>">
+            <label>Selecteer een profiel foto</label>
+            <input type="file" name="avatar" accept="image/*" class="form-control" value="<?php echo $avatar; ?>" required>
+            <span class="help-block"><?php echo $avatar_err; ?></span>
         </div>
         <div class="form-group">
             <input type="submit" class="btn btn-primary" value="Submit">
