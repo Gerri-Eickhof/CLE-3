a277b85ef9ab3c4ee61bb40f66928c756dc84ba1
TimoWieme <1004149@hr.nl>
2021-03-16 15:00:43 +0100

    Register page added

--- included/db-connection.php
+++ included/db-connection.php
@@ -1,27 +1,14 @@
 <?php
-//making the connection db so you don't have to change every file only this one
-function openCon() {
-
     $dbHost = "localhost"; //host of server
     $dbUser = "root"; //user
     $dbPass = ""; //password of user
     $db = "cle3-db"; //name of database
 
     //connecting to the database's
-    $conn = new mysqli($dbHost, $dbUser, $dbPass, $db) or die("Connect Failed; %s\n".
-        $conn -> error);
+    $link = mysqli_connect($dbHost, $dbUser, $dbPass, $db);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        echo "Connected Failed" ."<br>";
-
+// Check connection
+    if($link === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
     }
-//    echo "Connected successfully" ."<br>";
-    return $conn;
-}
-// closing connection to database
-function closeCon($conn){
-    $conn -> close();
-}
 ?>
