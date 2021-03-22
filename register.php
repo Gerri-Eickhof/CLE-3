705a7acba9bbb62cce5fdebb00ce004a7f2369c1
TimoWieme <1004149@hr.nl>
2021-03-22 11:01:32 +0100

    Afbeeldingen kunnen nu geupload worden in register.php

--- register.php
+++ register.php
@@ -60,29 +60,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $confirm_password_err = "Het wachtwoord komt niet overeen.";
         }
 
-//        // Validate profile picture
-//        if(empty(trim($_POST["avatar"]))){
-//            $avatar_err = "Voeg een profielfoto toe.";
-//        } else{
-//            $avatar = trim($_POST["avatar"]);
-//        }
+        // Validate profile picture
+        if(empty(trim($_POST["avatar"]))){
+            $avatar_err = "Voeg een profielfoto toe.";
+        } else{
+            $avatar = trim($_POST["avatar"]);
+        }
 
     }
 
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($avatar_err)){
 
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username, password, avatar) VALUES (?, ?, ?)";
 
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_password, $param_avatar);
 
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-//            $param_avatar = $avatar;
+            $param_avatar = $avatar;
 
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
@@ -106,13 +106,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Sign Up</title>
+    <title>Registreer </title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 <div class="wrapper">
-    <h2>Sign Up</h2>
+    <h2>Registreer</h2>
     <p>Vult het formulier in om een account te maken.</p>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
@@ -130,11 +130,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
             <span class="help-block"><?php echo $confirm_password_err; ?></span>
         </div>
-<!--        <div class="avatar --><?php //echo (!empty($avatar_err)) ? 'has-error' : ''; ?><!--">-->
-<!--            <label>Selecteer een profiel foto</label>-->
-<!--            <input type="file" name="avatar" accept="image/*" class="form-control" value="--><?php //echo $avatar; ?><!--" required>-->
-<!--            <span class="help-block">--><?php //echo $avatar_err; ?><!--</span>-->
-<!--        </div>-->
+        <div class="avatar <?php echo (!empty($avatar_err)) ? 'has-error' : ''; ?>">
+            <label>Selecteer een profiel foto</label>
+            <input type="file" name="avatar" accept="image/*" class="form-control" value="<?php echo $avatar; ?>" required>
+            <span class="help-block"><?php echo $avatar_err; ?></span>
+        </div>
         <div class="form-group">
             <input type="submit" class="btn btn-primary" value="Submit">
             <input type="reset" class="btn btn-default" value="Reset">
