052dcc614debea22bca1fdc9cea37b447d3930d9
TimoWieme <1004149@hr.nl>
2021-03-31 10:39:10 +0200

    Email adress toegevoegd in het registreer formulier

--- register.php
+++ register.php
@@ -3,8 +3,8 @@
 require_once "included/db-connection.php";
 
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = $avatar = "";
-$username_err = $password_err = $confirm_password_err = $avatar_err = "";
+$username = $email = $password = $confirm_password = $avatar = "";
+$username_err = $email_err = $password_err = $confirm_password_err = $avatar_err = "";
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -42,6 +42,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
 
+    // Validate Email
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Vul een email in.";
+    } else{
+        $email = trim($_POST["email"]);
+    }
+
     // Validate password
     if(empty(trim($_POST["password"]))){
         $password_err = "Voer een wachtwoord in.";
@@ -70,17 +77,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($avatar_err)){
+    if(empty($username_err) && empty($email_err) && empty($password_err) && empty($confirm_password_err) && empty($avatar_err)){
 
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password, avatar) VALUES (?, ?, ?)";
+        $sql = "INSERT INTO users (username, email, password, avatar) VALUES (?, ?, ?, ?)";
 
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_password, $param_avatar);
+            mysqli_stmt_bind_param($stmt, "ssss", $param_username, $param_email, $param_password, $param_avatar);
 
             // Set parameters
             $param_username = $username;
+            $param_email = $email;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
             $param_avatar = $avatar;
 
@@ -119,6 +127,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <label>Gebruikersnaam</label>
             <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
             <span class="help-block"><?php echo $username_err; ?></span>
+        </div>
+        <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+            <label>Email adress</label>
+            <input type="email" name="email" class="form-control" value="<?php echo $email; ?>">
+            <span class="help-block"><?php echo $email_err; ?></span>
         </div>
         <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
             <label>Wachtwoord</label>
