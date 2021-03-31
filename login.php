3c767156cb4206fcb1b4dc1caf3b20bf0f6b54bf
TimoWieme <1004149@hr.nl>
2021-03-31 10:28:03 +0200

    Tekst veranderd naar nederlands in login formulier

--- login.php
+++ login.php
@@ -105,12 +105,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <p>Vul uw gegevens in om in te loggen.</p>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-            <label>Username</label>
+            <label>Gebruikersnaam</label>
             <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
             <span class="help-block"><?php echo $username_err; ?></span>
         </div>
         <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-            <label>Password</label>
+            <label>Wachtwoord</label>
             <input type="password" name="password" class="form-control">
             <span class="help-block"><?php echo $password_err; ?></span>
         </div>
